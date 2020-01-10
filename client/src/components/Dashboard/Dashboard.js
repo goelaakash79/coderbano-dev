90219cd43dfea44b6b63003625ab77afd080ab6b
Aakash Goel <goelaakash79@gmail.com>
2020-01-11 02:14:09 +0530

    static ui added

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,68 +1,76 @@
 import React from "react";
 import "./style.css";
 // import { Link } from "react-router-dom";
-import { FaLock, FaKeyboard } from "react-icons/fa";
+import { FaLock, FaKeyboard, FaGhost } from "react-icons/fa";
 
 function Dashboard() {
+
+  const handleStalkDost = () => {
+    let {history} = this.props;
+
+    history.push("/stalk-friend")
+  }
   return (
-    <div class="container">
-      <h4 class="mt-5 fontBd">Dashboard</h4>
+    <div className="container">
+      <h4 className="mt-5 fontBd">Dashboard 
+      <span className="stalk-friend-dashboard" onClick={handleStalkDost}><FaGhost/> Stalk your friend</span>
+      </h4>
       <hr />
-      <div class="row mt-5">
-        <div class="col-md-9">
-          <h5 class="fontMd">Codeforces Ladders</h5>
-          <div class="row mt-4">
-            <div class="col-md-6">
-              <div class="card ladder-card p-4">
-                <h5 class="fontBd mb-2">Div. 2.A</h5>
-                <p class="desc">
+      <div className="row mt-5">
+        <div className="col-md-9">
+          <h5 className="fontMd">Codeforces Ladders</h5>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <div className="card ladder-card p-4">
+                <h5 className="fontBd mb-2">Div. 2.A</h5>
+                <p className="desc">
                   This is a good practice for whoever is beginner in programming
                   problems.
                 </p>
-                <button class="col-6 unlock-button">
+                <button className="col-6 unlock-button">
                   Unlock <FaLock />
                 </button>
               </div>
             </div>
 
-            <div class="col-md-6">
-              <div class="card ladder-card continue-div p-4">
-                <h5 class="fontBd mb-2">Div. 2.A</h5>
-                <p class="desc fontMd">
+            <div className="col-md-6">
+              <div className="card ladder-card continue-div p-4">
+                <h5 className="fontBd mb-2">Div. 2.A</h5>
+                <p className="desc fontMd">
                   Problems Solved: 09 out of 100
                   <br />
                   Last question solved:{" "}
-                  <span class="highlight">8 days ago</span>
+                  <span className="highlight">8 days ago</span>
                 </p>
-                <button class="col-6 unlock-button">
+                <button className="col-6 unlock-button">
                   Continue <FaKeyboard />
                 </button>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-md-3">
-          <h5 class="fontMd">Stats</h5>
+        <div className="col-md-3">
+          <h5 className="fontMd">Stats</h5>
 
-          <div class="stats-wrapper mt-4">
+          <div className="stats-wrapper mt-4">
             <h6>Problems Solved</h6>
             <h3>
-              <span class="fontBd green">79</span> / 800
+              <span className="fontBd green">79</span> / 800
             </h3>
             <br/>
             <h6>Joined</h6>
             <h5>
-              <span class="fontMd">Jan 05, 2020</span>
+              <span className="fontMd">Jan 05, 2020</span>
             </h5>
             <br/>
             <h6>Most Productive On</h6>
             <h5>
-              <span class="fontMd">Monday</span>
+              <span className="fontMd">Monday</span>
             </h5>
             <br/>
             <h6>Mostly codes in</h6>
             <h5>
-              <span class="fontMd">Night</span>
+              <span className="fontMd">Night</span>
             </h5>
           </div>
         </div>
--- client/src/components/Dashboard/Ladder.js
+++ client/src/components/Dashboard/Ladder.js
@@ -3,8 +3,15 @@ import React from "react";
 function Ladder() {
 	return (
 		<div className="container">
-			<h4 class="mt-5 fontBd">Ladder</h4>
+			<h4 className="mt-5 fontBd">Ladder: <span className="div-name">Div. 2.A</span></h4>
 			<hr />
+			<div className="desc">
+				<p>
+					The problems are sorted, the most solved problem comes
+					first. This is a good practice for whoever is beginner in
+					programming problems.
+				</p>
+			</div>
 			<div className="questions-section mb-4">
 				<table className="table">
 					<thead>
@@ -19,17 +26,13 @@ function Ladder() {
 						{/* {this.state.allQuestions.map((question, i) => { */}
 						{/* return ( */}
 						<tr key="hi">
-							<td>
-								1
-							</td>
+							<td>1</td>
 							<td>Code The God (Nice problem)</td>
 							<td>1</td>
 							<td>Solve usestion 1</td>
 						</tr>
 						<tr key="hi">
-							<td>
-								1
-							</td>
+							<td>1</td>
 							<td>Code The God (Nice problem)</td>
 							<td>1</td>
 							<td>Solve usestion 1</td>
