0460f62d74dd487d81c01ef458d344d51793d043
Aakash Goel <goelaakash79@gmail.com>
2020-01-11 00:53:03 +0530

    ladder ui added

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Register from "./components/Authentication/Register";
 import Login from "./components/Authentication/Login";
 
 import Dashboard from "./components/Dashboard/Dashboard";
-
+import Ladder from "./components/Dashboard/Ladder";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
         <Route exact path="/register" component={Register}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/" component={Dashboard}/>
+        <Route exact path="/ladder" component={Ladder}/>
       </Switch>
     </Router>
   );
