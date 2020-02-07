767c7a9088397a5a1a2a4a0a98aeb1861273ae9a
Aakash Goel <goelaakash79@gmail.com>
2020-02-07 23:58:41 +0530

    --update: loading added

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
-import { FaKeyboard, FaLock, FaGhost } from "react-icons/fa";
+import { FaKeyboard, FaLock, FaGhost, FaSpinner } from "react-icons/fa";
 import { dashboardService } from "../../utils/Services";
 
 const Dashboard = props => {
+	const [isLoading, setIsLoading] = useState(true);
 	const [isSuffData, setIsSuffData] = useState(false);
 	const [stats, setStats] = useState({
 		productiveDay: "",
@@ -18,7 +19,6 @@ const Dashboard = props => {
 	useEffect(() => {
 		(async () => {
 			const res = await dashboardService();
-			console.log(res);
 			let laddersArr = Object.keys(res.ladderDetails).map(
 				(ladder, id) => {
 					return {
@@ -33,8 +33,7 @@ const Dashboard = props => {
 			laddersArr.map((ladder, i) => {
 				return (problemsSolved += ladder.details.problemsSolved);
 			});
-
-			console.log(laddersArr);
+			setIsLoading(false);
 			if (res.most.productiveDay === "Insufficient data") {
 				setIsSuffData(true);
 			} else {
@@ -55,116 +54,139 @@ const Dashboard = props => {
 	};
 	return (
 		<div className="container">
-			<h4 className="mt-5 fontBd">
-				Dashboard
-				<span
-					className="stalk-friend-dashboard"
-					onClick={handleStalkDost}
-				>
-					<FaGhost /> Stalk your friend
-				</span>
-			</h4>
-			<hr />
-			<div className="row mt-5">
-				<div className="col-md-9">
-					<h5 className="fontMd">Codeforces Ladders</h5>
-					<div className="row mt-4">
-						{ladders.map((ladder, idx) => {
-							return (
-								<div className="col-md-6 mb-4" key={idx}>
-									<div
-										className={`card ladder-card p-4 ${
-											ladder.details.unlocked === true
-												? "continue-div"
-												: ""
-										}`}
-									>
-										<h5 className="fontBd mb-2">
-											Div. {ladder.name}
-										</h5>
-										<p className="desc">
-											{ladder.details.unlocked ===
-											true ? (
-												<>
-													Last Problem Solved on:{" "}
-													{
-														ladder.details.lastActivity.split(
-															"T"
-														)[0]
-													}
-												</>
-											) : (
-												<>
-													Unlock this ladder by
-													solving a problem
-												</>
-											)}
-										</p>
-										<Link
-											className="col-6 unlock-button"
-											to={{
-												pathname: `/ladder/${ladder.name}`,
-												state: { div: ladder.name }
-											}}
+			<div
+				className="loader text-center"
+				style={{
+					marginTop: "16em"
+				}}
+				hidden={!isLoading}
+			>
+				<p className="text-center">
+					<FaSpinner />
+					<br />
+					<span className="small">Loading</span>
+				</p>
+			</div>
+			<div className="section" hidden={isLoading}>
+				<h4 className="mt-5 fontBd">
+					Dashboard
+					<span
+						className="stalk-friend-dashboard"
+						onClick={handleStalkDost}
+					>
+						<FaGhost /> Stalk your friend
+					</span>
+				</h4>
+				<hr />
+				<div className="row mt-5">
+					<div className="col-md-9">
+						<h5 className="fontMd">Codeforces Ladders</h5>
+						<div className="row mt-4">
+							{ladders.map((ladder, idx) => {
+								return (
+									<div className="col-md-6 mb-4" key={idx}>
+										<div
+											className={`card ladder-card p-4 ${
+												ladder.details.unlocked === true
+													? "continue-div"
+													: ""
+											}`}
 										>
-											{ladder.details.unlocked ===
-											true ? (
-												<>
-													Continue <FaKeyboard />
-												</>
-											) : (
-												<>
-													Unlock <FaLock />
-												</>
-											)}
-										</Link>
+											<h5 className="fontBd mb-2">
+												Div. {ladder.name}
+											</h5>
+											<p className="desc">
+												{ladder.details.unlocked ===
+												true ? (
+													<>
+														Last Problem Solved on:{" "}
+														{
+															ladder.details.lastActivity.split(
+																"T"
+															)[0]
+														}
+													</>
+												) : (
+													<>
+														Unlock this ladder by
+														solving a problem
+													</>
+												)}
+											</p>
+											<Link
+												className="col-6 unlock-button"
+												to={{
+													pathname: `/ladder/${ladder.name}`,
+													state: { div: ladder.name }
+												}}
+											>
+												{ladder.details.unlocked ===
+												true ? (
+													<>
+														Continue <FaKeyboard />
+													</>
+												) : (
+													<>
+														Unlock <FaLock />
+													</>
+												)}
+											</Link>
+										</div>
 									</div>
-								</div>
-							);
-						})}
+								);
+							})}
+						</div>
 					</div>
-				</div>
-				<div className="col-md-3">
-					<h5 className="fontMd">Stats</h5>
+					<div className="col-md-3">
+						<h5 className="fontMd">Stats</h5>
 
-					<div className="stats-wrapper mt-4" hidden={isSuffData}>
-						<h6>Problems Solved</h6>
-						<h3>
-							<span className="fontBd green">
-								{stats.problemsSolved}
-							</span>{" "}
-							/ 800
-						</h3>
-						<br />
-						<h6>Most Productive On</h6>
-						<h5>
-							<span className="fontMd">
-								{stats.productiveDay}
-							</span>
-						</h5>
-						<br />
-						<h6>Mostly codes in</h6>
-						<h5>
-							<span className="fontMd">
-								{stats.productiveTimeOfDay}
-							</span>
-						</h5>
-						<br />
-						<h6>Mostly codes in</h6>
-						<h5>
-							<span className="fontMd">{stats.usedLanguage}</span>
-						</h5>
-						<br />
-						<h6>Joined on</h6>
-						<h5>
-							<span className="fontMd">
-								{stats.joined.split("T")[0]}
-							</span>
-						</h5>
-					</div>
+						<div className="stats-wrapper mt-4" hidden={isSuffData}>
+							<h6>Problems Solved</h6>
+							<h3>
+								<span className="fontBd green">
+									{stats.problemsSolved}
+								</span>{" "}
+								/ 800
+							</h3>
+							<br />
+							<h6>Most Productive On</h6>
+							<h5>
+								<span className="fontMd">
+									{stats.productiveDay}
+								</span>
+							</h5>
+							<br />
+							<h6>Mostly codes in</h6>
+							<h5>
+								<span className="fontMd">
+									{stats.productiveTimeOfDay}
+								</span>
+							</h5>
+							<br />
+							<h6>Mostly codes in</h6>
+							<h5>
+								<span className="fontMd">
+									{stats.usedLanguage}
+								</span>
+							</h5>
+							<br />
+							<h6>Joined on</h6>
+							<h5>
+								<span className="fontMd">
+									{stats.joined.split("T")[0]}
+								</span>
+							</h5>
+						</div>
 
-					<div className="stats-wrapper mt-4" hidden={!isSuffData}>
-						Please unlock a ladder to view stats "😀"
+						<div
+							className="stats-wrapper mt-4"
+							hidden={!isSuffData}
+						>
+							Please unlock a ladder to view stats{" "}
+							<span role="img" aria-label="">
+								😀
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
